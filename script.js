41790c64b4d742cafecbd01c6bb692f7c30d5d32
Chris <ffson1999@yahoo.com>
2024-05-24 12:27:23 -0400

    --Return to this Saving changes of calculator javascript.

--- script.js
+++ script.js
@@ -5,23 +5,75 @@ const dotButton = document.getElementById("dot-btn");
 const numberButtons = document.querySelectorAll(".btn-number");
 const operatorButtons = document.querySelectorAll(".btn-operator");
 
+let firstNum = "";
+let secondNum = "";
+let currentOperator = null;
+let shouldResetScreen = false;
 
-clearButton.addEventListener("click", clearScreen);
-numberButtons.forEach(button => {
+equalsButton.addEventListener("click", calculate)
+clearButton.addEventListener("click", clearScreen)
+dotButton.addEventListener("click", appendDot)
+
+//Sets text content to 0 to remove bug on inital load of page.
+calculatorScreen.textContent = "0";
+
+numberButtons.forEach((button) => {
   button.addEventListener("click", () => appendNumber(button.textContent))
 })
 
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", () => operatorInput(button.textContent))
+})
+
+function appendNumber(number) {
+  if (calculatorScreen.textContent === "0" || shouldResetScreen)
+    resetScreen();
+  calculatorScreen.textContent += number;
+  console.log(calculatorScreen.textContent);
+}
+
+function resetScreen() {
+  calculatorScreen.textContent = "";
+  shouldResetScreen = false;
+}
 
 function clearScreen() {
   calculatorScreen.textContent = "0";
+  firstNum = "";
+  secondNum = "";
+  currentOperator = null;
 }
 
-function appendNumber(number) {
-  if (calculatorScreen.textContent === "0") {
-    calculatorScreen.textContent = number;
-  } else {
-    calculatorScreen.textContent += number;
+function appendDot() {
+  if (shouldResetScreen) resetScreen();
+  if (calculatorScreen.textContent === "")
+    calculatorScreen.textContent = "0"
+  if (calculatorScreen.textContent.includes(".")) return;
+  calculatorScreen.textContent += ".";
+}
+
+function operatorInput(operator) {
+  if (currentOperator !== null) calculate();
+  firstNum = calculatorScreen.textContent;
+  currentOperator = operator;
+  shouldResetScreen = true;
+}
+
+function calculate() {
+  if (currentOperator === null || shouldResetScreen) return;
+  if (currentOperator === "÷" && calculatorScreen.textContent === "0") {
+    alert("You can't divide by 0 silly!");
+    return;
   }
+  secondNum = calculatorScreen.textContent;
+  calculatorScreen.textContent = roundResult(
+    operate(currentOperator, firstNum, secondNum)
+  )
+  currentOperator = null;
+}
+
+function roundResult(number) {
+  return Math.round(number * 1000) / 1000;
 }
 
 //Operate function to calculate equation
@@ -37,9 +89,9 @@ function operate(operator, x, y) {
             return add(x, y)
         case "-":
             return subtract(x, y)
-        case "*":
+        case "×":
             return multiply(x, y)
-        case "/":
+        case "÷":
             return division(x, y)
     }   
 }
